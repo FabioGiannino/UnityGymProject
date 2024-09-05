790c62362fc742851397652532ae4846d413cae0
FabioGiannino <fabiogiannino2@gmail.com>
2024-09-05 12:08:34 +0200

    Add new NPC that repeat his path

--- Assets/Scripts/Agents/Enemies/SimpleTriangleEnemy3.cs
+++ Assets/Scripts/Agents/Enemies/SimpleTriangleEnemy3.cs
@@ -2,28 +2,8 @@ using UnityEngine;
 using FSM;
 using System.Linq;
 
-public class DestinationMarks
-{
-    private Transform transform;
-    private BoolWrapper marked;
-    public Transform Transform {  get { return transform; } }
-    public BoolWrapper Marked { get { return marked; } }
-    public DestinationMarks(Transform destination, BoolWrapper marked )
-    {
-        this.transform = destination;
-        this.marked = marked;
-    }
-    public void CheckDestination()
-    {
-        marked.Value = true;
-    }
-    public void UncheckDestination()
-    {
-        marked.Value = false;
-    }
-}
 
-public class SimpleTriangleEnemy2 : MonoBehaviour
+public class SimpleTriangleEnemy3 : MonoBehaviour
 {
     [SerializeField]
     private Transform[] points;
@@ -32,34 +12,35 @@ public class SimpleTriangleEnemy2 : MonoBehaviour
     [SerializeField]
     private float timeWait;
 
-    private DestinationMarks[] destinations;
+    private Destination[] destinations;
 
     private State IdleStateSetup(Rigidbody2D rigidbody)
     {
         State idle = new State("IdleState");
-        StateAction setVelocity = new SetVelocity2DAction(rigidbody, Vector2.zero);        
-        idle.SetUpMe(new StateAction[] { setVelocity });    
+        StateAction setVelocity = new SetVelocity2DAction(rigidbody, Vector2.zero);
+        StateAction resetAllDestination = new ResetDestinationUnmarkedAction(destinations);
+        idle.SetUpMe(new StateAction[] { setVelocity , resetAllDestination });
         return idle;
     }
-    private State MoveStateSetup(Rigidbody2D rigidbody, DestinationMarks destination, int i)
+    private State MoveStateSetup(Rigidbody2D rigidbody, Destination destination, int i)
     {
-        State move = new State("MoveState_"+i);
-        StateAction setVelocity = new SetVelocity2DAction(rigidbody, velocity,true);
+        State move = new State("MoveState_" + i);
+        StateAction setVelocity = new SetVelocity2DAction(rigidbody, velocity, true);
         StateAction moveToDestination = new GoToPosition2DAction(gameObject, destination.Transform);
         StateAction setIsMarked = new SetBoolVariableAction(destination.Marked, true);
-        move.SetUpMe(new StateAction[] { setVelocity , setIsMarked, moveToDestination  });
+        move.SetUpMe(new StateAction[] { setVelocity, setIsMarked, moveToDestination });
         return move;
     }
 
-    private Transition IdleToMove(State prev, State next, DestinationMarks destination)
+    private Transition IdleToMove(State prev, State next, Destination destination)
     {
         Transition transition = new Transition();
         Condition timeCondition = new ExitTimeCondition(timeWait);
         Condition checkIsNotMarked = new CheckBoolCondition(destination.Marked, false);
-        transition.SetUpMe(prev,next,new Condition[] { timeCondition, checkIsNotMarked });
+        transition.SetUpMe(prev, next, new Condition[] { timeCondition, checkIsNotMarked });
         return transition;
     }
-        
+
 
     private Transition MoveToIdle(State prev, State next, Transform destination)
     {
@@ -74,22 +55,24 @@ public class SimpleTriangleEnemy2 : MonoBehaviour
         StateMachine machine = GetComponent<StateMachine>();
         Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
 
-        destinations = new DestinationMarks[points.Length];
+        destinations = new Destination[points.Length];
         State[] moveTo = new State[points.Length];
         Transition[] movesToIdle = new Transition[destinations.Length];
         Transition[] idleToMove = new Transition[destinations.Length];
-        
+        for (int i = 0; i < points.Length; i++)
+        {
+            destinations[i] = new Destination(points[i], new BoolWrapper(false));
+        }
 
         //setup idle state
         State idle = IdleStateSetup(rigidbody);
         for (int i = 0; i < points.Length; i++)
         {
-            destinations[i] = new DestinationMarks(points[i], new BoolWrapper(false));
             //Setup move states
             moveTo[i] = MoveStateSetup(rigidbody, destinations[i], i);
-            
+
             //set transitions for each move states to idle and idle to move
-            movesToIdle[i]= MoveToIdle(moveTo[i], idle, destinations[i].Transform);
+            movesToIdle[i] = MoveToIdle(moveTo[i], idle, destinations[i].Transform);
             idleToMove[i] = IdleToMove(idle, moveTo[i], destinations[i]);
 
             //set the specific transition to idle for each move state
@@ -97,7 +80,7 @@ public class SimpleTriangleEnemy2 : MonoBehaviour
         }
         //set all possible transitions to move for the only idle state
         idle.SetUpMe(idleToMove);
-        State[] states = moveTo.Concat(new State[] { idle }).ToArray();        
+        State[] states = moveTo.Concat(new State[] { idle }).ToArray();
         machine.Init(states, idle);
 
     }
---
+++ Assets/Scripts/FSM/StateActions/Destination/ResetDestinationsUnmarkedAction.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+public class Destination
+{
+    private Transform transform;
+    private BoolWrapper marked;
+    public Transform Transform { get { return transform; } }
+    public BoolWrapper Marked { get { return marked; } }
+    public Destination(Transform destination, BoolWrapper marked)
+    {
+        this.transform = destination;
+        this.marked = marked;
+    }
+    public void MarkDestination(bool value)
+    {
+        marked.Value = value;
+    }
+
+}
+namespace FSM
+{
+    public class ResetDestinationUnmarkedAction: StateAction
+    {
+        Destination[] destinations;
+
+        /// <summary>
+        /// FSM Action: Checked if all destinations are marked yet and, if is true, unmarks all
+        /// </summary>
+        /// <param name="destinations">All possible destinations</param>
+        public ResetDestinationUnmarkedAction(Destination[] destinations)
+        {
+            this.destinations = destinations;
+        }
+        public override void OnEnter()
+        {
+            if(!AllDestinationAreDiscovered()) return; 
+            ResetAllDestinations();
+        }
+
+
+        private bool AllDestinationAreDiscovered()
+        {
+            foreach (var dest in destinations)
+            {
+                if (dest.Marked.Value == false)
+                {
+                    return false;
+                }
+
+            }
+            return true;
+        }
+
+        private void ResetAllDestinations()
+        {
+            foreach (var dest in destinations)
+            {
+                dest.Marked.Value = false;
+            }
+        }
+    }
+
+}
